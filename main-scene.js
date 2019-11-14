fe8aaddb1646cf22ed38bf6ee679a1169c69ca1d
amandaliyuan <aliyuanh@gmail.com>
2019-11-14 14:26:47 -0800

    updated sun shader
    sun shader with fake mapping, perlin noise algorithm

--- main-scene.js
+++ main-scene.js
@@ -51,6 +51,7 @@ class Solar_System extends Scene
                                                               // Extra credit shaders:
       const black_hole_shader = new Black_Hole_Shader();
       const sun_shader        = new Sun_Shader();
+      const fun_shader = new defs.Funny_Shader();
       
                                               // *** Materials: *** wrap a dictionary of "options" for a shader.
 
@@ -68,7 +69,7 @@ class Solar_System extends Scene
                                       ambient: 0, diffusivity: 1, specularity: 1, color: Color.of( .4,.4,.4,1 ) } ),
                       black_hole: new Material( black_hole_shader ),
                              sun_mat: new Material( phong_shader,
-                                    { ambient: 1, diffusivity: 1, specularity: .8, color: Color.of( 0,0,0,1 ) } ),
+                                    { ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0,1 ) } ),
                              rock: new Material( phong_shader,
                                     { ambient: 0, diffusivity: .5, specularity: 1, color: Color.of( .4,.4,.4,1 ) } ),
                              m2: new Material( gouraud_shader,
@@ -83,6 +84,9 @@ class Solar_System extends Scene
                           starBoi: new Material(texture_shader_2, {texture: new Texture("assets/star_face.png"), ambient: 1, 
                             diffusivity:1, specularity:1, color: Color.of(0,0,0,1)}),
                            moonBoi : new Material(phong_shader, {ambient:0,diffusivity:.8, specularity:.5, smoothness:10, color: Color.of(1,1,1,1)}),
+                          funBoi : new Material(fun_shader, {ambient:0,diffusivity:.8, specularity:.5, smoothness:10, color: Color.of(1,1,1,1)}),
+                          blackHole: new Material(black_hole_shader, {ambient: 1, diffusivity: .5, specularity: .5, color: Color.of(.7,.3,.3,1)}),
+                          bestSun: new Material(sun_shader, {ambient:1, diffusivity: .5, specularity: .5, color: Color.of(.5,.5,.5,.5)}),
 
                        };
 
@@ -178,7 +182,7 @@ class Solar_System extends Scene
                                                 // TODO (#3c):  Replace with a point light located at the origin, with the sun's color
                                                 // (created above).  For the third argument pass in the point light's size.  Use
                                                 // 10 to the power of sun_size.
-      program_state.lights = [ new Light( Vec.of( 0,0,0,1 ), sun_color, 10**(sun_size)) ];
+      program_state.lights = [ new Light( Vec.of( 0,0,0,1 ), sun_color, 10000000**(sun_size)) ];
 
                             // TODO (#5c):  Throughout your program whenever you use a material (by passing it into draw),
                             // pass in a modified version instead.  Call .override( modifier ) on the material to
@@ -204,7 +208,7 @@ class Solar_System extends Scene
       //this.shapes.ball_4.draw( context, program_state, model_transform, this.materials.metal_earth.override( blue ) );
       //SUN!
       model_transform = model_transform.post_multiply(sun);
-      this.shapes.ball_6.draw(context,program_state,model_transform,this.materials.sun_mat);
+      this.shapes.ball_6.draw(context,program_state,model_transform,this.materials.bestSun.override(sun_color));
       //first planet
       if(this.lights_on){
        this.materials.rock =  this.materials.rock.override({ambient:.7});
@@ -234,12 +238,11 @@ class Solar_System extends Scene
       model_transform = model_transform.post_multiply(Mat4.translation([6,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
       this.shapes.ball_3.draw(context,program_state,model_transform,this.materials.rock);
-       model_transform = model_transform.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
+      //model_transform = model_transform.post_multiply(Mat4.rotation(-Math.PI/2,[0,1,0]));
+      model_transform = model_transform.post_multiply(Mat4.translation([-3,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(-Math.PI/2,[0,1,0]));
 
-      model_transform = model_transform.post_multiply(Mat4.translation([0,0,-3]));
-
-            //this.shapes.ball_3.draw(context,program_state,model_transform,this.materials.plastic);
+           // this.shapes.ball_3.draw(context,program_state,model_transform,this.materials.plastic);
 
       this.camera_teleporter.cameras.push( Mat4.inverse( model_transform.copy()));
       //second planet - shiny mirror 
@@ -248,22 +251,35 @@ class Solar_System extends Scene
       model_transform = model_transform.post_multiply(Mat4.translation([9,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
       this.shapes.ball_2.draw(context,program_state,model_transform,this.materials.m2);
-       model_transform = model_transform.post_multiply(Mat4.translation([-2.5,0,0]));
-      model_transform = model_transform.post_multiply(Mat4.rotation(-Math.PI/2,[0,1,0]));
-      this.camera_teleporter.cameras.push( Mat4.inverse( model_transform.copy()));
-     
+            var camThing = model_transform.copy();
+       camThing =camThing.post_multiply(Mat4.translation([-3,0,0]));
+      camThing = camThing.post_multiply(Mat4.rotation(-Math.PI/2,[0,1,0]));
+      this.camera_teleporter.cameras.push( Mat4.inverse( camThing.copy()));
+      //moon number 1!!!!!
+      model_transform = model_transform.post_multiply(Mat4.rotation(t/3,[0,1,0]));
+      model_transform = model_transform.post_multiply(Mat4.translation([2,0,0]));
+      model_transform = model_transform.post_multiply(Mat4.scale([.75,.75,.75]));
+      //this.shapes.ball_6.draw(context, program_state, model_transform, this.materials.funBoi);
+      this.shapes.ball_6.draw(context, program_state, model_transform, this.materials.blackHole);
+      var camThing = model_transform.copy();
+      camThing = camThing.post_multiply(Mat4.scale([1/.75,1/.75,1/.75]));
+      camThing = camThing.post_multiply(Mat4.translation([0,0,2.5]));
+      this.camera_teleporter.cameras.push( Mat4.inverse( camThing.copy()));
       //third planet -- earth! 
       model_transform = origin_system.copy();
       model_transform = model_transform.post_multiply(Mat4.rotation(t/2, [0,1,0]));
       model_transform = model_transform.post_multiply(Mat4.translation([14,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
       this.shapes.ball_4.draw(context,program_state,model_transform,this.materials.earth);
-      model_transform = model_transform.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
-      model_transform = model_transform.post_multiply(Mat4.translation([0,0,2]));
-      this.camera_teleporter.cameras.push( Mat4.inverse( model_transform.copy()));
+      var camThing = model_transform.copy();
+      camThing = camThing.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
+      camThing = camThing.post_multiply(Mat4.translation([0,0,1.5]));
+      this.camera_teleporter.cameras.push( Mat4.inverse( camThing.copy()));
+      //model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
+      //model_transform = model_transform.post_multiply(Mat4.translation([0,0,-1.5]));
       //moon 2!!!!!!!!
        model_transform = model_transform.post_multiply(Mat4.rotation(t/3.5, [0,1,0]));
-      model_transform = model_transform.post_multiply(Mat4.translation([1.5,0,0]));
+      model_transform = model_transform.post_multiply(Mat4.translation([2,0,0]));
       model_transform = model_transform.post_multiply(Mat4.scale([.5,.5,.5]));
       this.shapes.ball_1.draw(context,program_state,model_transform,this.materials.moonBoi);
       model_transform = model_transform.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
@@ -275,12 +291,18 @@ class Solar_System extends Scene
       model_transform = model_transform.post_multiply(Mat4.translation([16,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
       this.shapes.ball_5.draw(context,program_state,model_transform,this.materials.brickBoi);
+      model_transform = model_transform.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
+      model_transform = model_transform.post_multiply(Mat4.translation([0,0,3]));
+      this.camera_teleporter.cameras.push( Mat4.inverse( model_transform.copy()));
       //fifth planet - good brick :) 
       model_transform = origin_system.copy();
       model_transform = model_transform.post_multiply(Mat4.rotation(t/4, [0,1,0]));
       model_transform = model_transform.post_multiply(Mat4.translation([19,0,0]));
       model_transform = model_transform.post_multiply(Mat4.rotation(t/3, [0,1,0]));
       this.shapes.ball_5.draw(context,program_state,model_transform,this.materials.brickBoiBetter);
+      model_transform = model_transform.post_multiply(Mat4.rotation(-t/3, [0,1,0]));
+      model_transform = model_transform.post_multiply(Mat4.translation([0,0,3]));
+      this.camera_teleporter.cameras.push( Mat4.inverse( model_transform.copy()));
 
 
     }
@@ -487,14 +509,19 @@ class Black_Hole_Shader extends Shader         // Simple "procedural" texture sh
                   // custom materials for this part so you don't need any values from the material object.
                   // For an example of how to send variables to the GPU, check out the simple shader "Funny_Shader".
 
-        // context.uniformMatrix4fv( gpu_addresses.projection_camera_model_transform,       
+        // context.uniformMatrix4fv( gpu_addresses.projection_camera_model_transform, 
+        const [ P, C, M ] = [ program_state.projection_transform, program_state.camera_inverse, model_transform ],
+                      PCM = P.times( C ).times( M );
+        context.uniformMatrix4fv( gpu_addresses.projection_camera_model_transform, false, Mat.flatten_2D_to_1D( PCM.transposed() ) );
+        context.uniform1f ( gpu_addresses.animation_time, program_state.animation_time / 1000 );      
       }
   shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
     { 
                   // TODO (#EC 1c):  For both shaders, declare a varying vec2 to pass a texture coordinate between
                   // your shaders.  Also make sure both shaders have an animation_time input (a uniform).
-      return `precision mediump float;
-             
+       return `precision mediump float;
+              varying vec2 f_tex_coord;
+              varying float disp;
       `;
     }
   vertex_glsl_code()           // ********* VERTEX SHADER *********
@@ -504,21 +531,34 @@ class Black_Hole_Shader extends Shader         // Simple "procedural" texture sh
                           // you have the current vertex's stored position and texture coord, animation time,
                           // and the final product of the projection, camera, and model matrices.
       return this.shared_glsl_code() + `
-
+        attribute vec3 position;                            // Position is expressed in object coordinates.
+        attribute vec2 texture_coord;
+        uniform mat4 projection_camera_model_transform;
+        uniform float animation_time;
         void main()
-        { 
-
-        }`;
+        {   // The vertex's final resting place (in NDCS).
+          f_tex_coord = texture_coord;   
+                                            // Directly use original texture coords and interpolate between.
+          float sin_value = mod( 5.*animation_time + f_tex_coord.y*30., radians(360.) );
+          float rate = 0.2* sin( sin_value ) + 1.2;
+          vec3 pose = vec3( position.x * rate, position.y * rate, position.z * rate);
+          gl_Position = projection_camera_model_transform * vec4( pose, 1.0);        }`;
     }
   fragment_glsl_code()           // ********* FRAGMENT SHADER *********
     { 
                           // TODO (#EC 1f):  Using the input UV texture coordinates and animation time,
                           // calculate a color that makes moving waves as V increases.  Store
                           // the result in gl_FragColor.
-      return this.shared_glsl_code() + `
+     return this.shared_glsl_code() + `
+        uniform float animation_time;
         void main()
-        { 
-
+        { float a = animation_time, u = f_tex_coord.x, v = f_tex_coord.y;   
+                                                                  // Use an arbitrary math function to color in all pixels as a complex                                                                  
+          gl_FragColor = vec4(                                    // function of the UV texture coordintaes of the pixel and of time.  
+            0.5 * sin(30.*v+a*5.),
+            0.9 * sin(30.*v+a*5.),
+            1.5 * sin(30.*v+a*5.),
+            1.);
         }`;
     }
 }
@@ -526,34 +566,184 @@ class Black_Hole_Shader extends Shader         // Simple "procedural" texture sh
 
 const Sun_Shader = defs.Sun_Shader =
 class Sun_Shader extends Shader
-{ update_GPU( context, gpu_addresses, graphics_state, model_transform, material )
+{ 
+
+send_material( gl, gpu, material )
+    {                                       // send_material(): Send the desired shape-wide material qualities to the
+                                            // graphics card, where they will tweak the Phong lighting formula.                                      
+      gl.uniform4fv( gpu.sun_color,    material.color       );
+      gl.uniform1f ( gpu.ambient,        material.ambient     );
+      gl.uniform1f ( gpu.diffusivity,    material.diffusivity );
+      gl.uniform1f ( gpu.specularity,    material.specularity );
+      gl.uniform1f ( gpu.smoothness,     material.smoothness  );
+    }
+
+
+update_GPU( context, gpu_addresses, program_state, model_transform, material )
     {
                       // TODO (#EC 2): Pass the same information to the shader as for EC part 1.  Additionally
                       // pass material.color to the shader.
-
-
+     const [ P, C, M ] = [ program_state.projection_transform, program_state.camera_inverse, model_transform ],
+                      PCM = P.times( C ).times( M );
+        context.uniformMatrix4fv( gpu_addresses.projection_camera_model_transform, false, Mat.flatten_2D_to_1D( PCM.transposed() ) );
+        context.uniform1f ( gpu_addresses.time, program_state.animation_time / 1000 );  
+        context.uniform1f(gpu_addresses.sun_color, material.color);  
+        const defaults = { color: Color.of( 0,0,0,1 ), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40 };
+        material = Object.assign( {}, defaults, material );
+        this.send_material ( context, gpu_addresses, material );
     }
                                 // TODO (#EC 2):  Complete the shaders, displacing the input sphere's vertices as
                                 // a fireball effect and coloring fragments according to displacement.
 
   shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
     { return `precision mediump float;
-                            
+              varying vec2 f_tex_coord;
+              varying float disp;
+              uniform vec4 sun_color;
+              varying float noise;
+
       `;
     }
   vertex_glsl_code()           // ********* VERTEX SHADER *********
+    {  return this.shared_glsl_code() + `
+       uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat3 normalMatrix;
+        uniform mat4 projection_camera_model_transform;
+
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+attribute vec2 uv2;
+
+uniform float time;
+ float fireSpeed = .5;
+float pulseHeight = 0.0;
+float displacementHeight = .5;
+float turbulenceDetail = .7;
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r) {
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Klassisk Perlin noise 
+float cnoise(vec3 P) {
+  vec3 Pi0 = floor(P); // indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
+// Ashima code 
+float turbulence( vec3 p ) {
+    float t = -0.5;
+    for (float f = 1.0 ; f <= 10.0 ; f++ ){
+        float power = pow( 2.0, f );
+        t += abs( cnoise( vec3( power * p ) ) / power );
+    }
+    return t;
+}
+
+void main() {
+    noise = -0.8 * turbulence( turbulenceDetail * normal + ( time * 1.0 ) );
+    
+    float b = pulseHeight * cnoise(
+        0.05 * position + vec3( 1.0 * time )
+    );
+    float displacement = ( 0.0 - displacementHeight ) * noise + b;
+    disp = displacement*30.;
+    vec3 newPosition = position + normal * displacement;
+    gl_Position = projection_camera_model_transform * vec4( newPosition, 2.0 );
+}
+       `;
+          }
+  fragment_glsl_code()           // ********* FRAGMENT SHADER *********
     { return this.shared_glsl_code() + `
 
         void main()
-        {
-
+        { 
+         vec3 color = vec3((1.-disp), (0.1-disp*0.2)+0.1, (0.1-disp*0.1)+0.1*abs(sin(disp)));
+        gl_FragColor = vec4( color.rgb, 1.0 );
+        gl_FragColor *= sun_color;
         }`;
-    }
-  fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    { return this.shared_glsl_code() + `
-        void main() 
-        {
-
-        } ` ;
     }
 }
